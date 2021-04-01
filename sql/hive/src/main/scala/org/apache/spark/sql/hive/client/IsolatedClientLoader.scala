56a801c25dbd7470d9bcaeb9e7029d4513a0dc79
Fabio Buso <buso.fabio@gmail.com>
2021-04-01 16:44:40 +0200

    Typo

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/IsolatedClientLoader.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/IsolatedClientLoader.scala
@@ -91,7 +91,7 @@ private[hive] object IsolatedClientLoader extends Logging {
   def hiveVersion(version: String): HiveVersion = {
     // Fabio: Remove the last digit of the version string as it's the Hopsworks specific version
     version.substring(0, version.lastIndexOf(".")) match {
-      case "12" | "0.12" | "0.12.0" => hive.v11
+      case "12" | "0.12" | "0.12.0" => hive.v12
       case "13" | "0.13" | "0.13.0" | "0.13.1" => hive.v13
       case "14" | "0.14" | "0.14.0" => hive.v14
       case "1.0" | "1.0.0" | "1.0.1" => hive.v1_0
